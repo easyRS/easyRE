782a7a5a191a1a2f1362c0f4eb0b2c1f5783db26
Luis Fernando Morón Dorado <luifermoron@Luiss-Mac-mini.local>
2022-05-03 16:07:22 -0400

    ci: implement commitlint

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello World!
         </h1>
 
         <p className={styles.description}>
