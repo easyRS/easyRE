a14b4717efdd8ff7bceee34657b210ed49b70714
Luis Fernando Moron <contact@luifermoron.com>
2022-07-08 17:36:02 -0400

    refactor: tojson

--- lib/utils/MongooseToJSON.tsx
+++ lib/utils/MongooseToJSON.tsx
@@ -9,7 +9,8 @@ const toJson = <T,>(obj: Document) => {
     transform: (_1: any, ret: any, _2: any): any => {
       const { _id: removedId, __v: removedV, ...newRet } = ret;
       return { _id: ret._id.toString(), ...newRet };
-    }
+    },
+    flattenMaps: false
   };
   return obj.toJSON<T>(options);
 };
