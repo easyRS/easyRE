86d5825335b5712ca8c235b972fbe6721e4af373
Luis Fernando Moron <contact@luifermoron.com>
2022-08-30 16:00:05 -0400

    refactor: implement abstractusecase class

---
+++ lib/useCases/AbstractUseCases.tsx
@@ -0,0 +1,44 @@
+import IRepository from '../domain/repositories/base/IRepository';
+
+export default abstract class AbstractUseCases<
+  Model,
+  BaseRepository extends IRepository<Model>
+> {
+  repository: IRepository<Model>;
+
+  constructor() {
+    this.repository = this.buildRepository();
+  }
+
+  abstract buildRepository(): BaseRepository;
+
+  abstract buildFrom(object: Record<string, unknown>): Model;
+
+  async create(object: Record<string, unknown>): Promise<Model> {
+    const newProperties: Model = this.buildFrom(object);
+    return this.repository.create(newProperties);
+  }
+
+  async update(object: Record<string, unknown>): Promise<void> {
+    const id = object._id as string;
+    const updatedProperty: Model = this.buildFrom(object);
+    return this.repository.findOneAndUpdate(id, updatedProperty);
+  }
+
+  async remove(object: Record<string, unknown>): Promise<void> {
+    const id = object._id as string;
+    return this.repository.removeById(id);
+  }
+
+  async list(): Promise<Model[]> {
+    return this.repository.list();
+  }
+
+  async findById(_id: string): Promise<Model> {
+    return this.repository.findById(_id);
+  }
+
+  async getKeys(): Promise<ModelKeys> {
+    return this.repository.getKeys();
+  }
+}
--- lib/useCases/PropertyUseCases.tsx
+++ lib/useCases/PropertyUseCases.tsx
@@ -1,9 +1,26 @@
 import IProperty from '../domain/entities/IProperty';
-import IRepository from '../domain/repositories/base/IRepository';
 import PropertyRepository from '../domain/repositories/PropertyRepository';
+import AbstractUseCases from './AbstractUseCases';
 
-export default class PropertyUseCases {
-  repository: IRepository<IProperty> = new PropertyRepository();
+export default class PropertyUseCases extends AbstractUseCases<
+  IProperty,
+  PropertyRepository
+> {
+  /* eslint-disable-line class-methods-use-this */ buildRepository(): PropertyRepository {
+    return new PropertyRepository();
+  }
+
+  /* eslint-disable-line class-methods-use-this */ buildObject(
+    object: Record<string, unknown>
+  ): IProperty {
+    return {
+      coordinates: object.coordinates as [number],
+      measure: object.measure as string,
+      name: object.name as string,
+      location_details: object.location_details as string,
+      description: object.description as string
+    };
+  }
 
   async create(object: Record<string, unknown>): Promise<IProperty> {
     const newProperties: IProperty = {
