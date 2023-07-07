a7d449681fbcda5ea4d5e90b3e0c07471abacce0
Luis Fernando Moron <contact@luifermoron.com>
2023-07-07 14:46:30 -0400

    fix: remove potential unused nginx.sh file

--- docker/prod/nginx/nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ ! -f /etc/nginx/conf.d/default.temp ]; then
-  touch /etc/nginx/conf.d/default.temp
-fi
-cp /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.temp
-envsubst '${NGINX_HOST}' < /etc/nginx/conf.d/default.temp > /etc/nginx/conf.d/default.conf
-
-exec "$@"
