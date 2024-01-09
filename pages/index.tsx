fbca9d663286e6854a35ec4b27830d9f9f767f46
Luis Fernando Moron <contact@luifermoron.com>
2024-01-09 15:48:04 -0400

    fix: add data folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ const Home: NextPage<IndexTaskProps> = (tasksProps: IndexTaskProps) => {
                 lineHeight: '2rem'
               }}
             >
-              Welcome7 to <strong>Easy RS</strong>!. The easy open-source way to
+              Welcome8 to <strong>Easy RS</strong>!. The easy open-source way to
               manage your Real State 🏠 stuff
               <br /> <FaRegLightbulb />
               &nbsp; Start creating a lease for managing your properties. Press
