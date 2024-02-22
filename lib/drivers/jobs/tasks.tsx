ec6116c07f793e5cb3c2489a64457d48c946a322
Luis Fernando Moron <contact@luifermoron.com>
2024-02-22 16:02:02 -0400

    fix: return value

--- lib/drivers/jobs/tasks.tsx
+++ lib/drivers/jobs/tasks.tsx
@@ -1,13 +1,14 @@
 import LeaseContractUseCases from '../../useCases/LeaseContractUseCases';
 import { daysBetween, monthsBetween } from '../../utils/datesHelper';
 
+import ILeaseContract from '../../domain/entities/ILeaseContract';
 import { TIME_TYPE_MONTHLY_OPTION } from '../../domain/entities/TimeType';
 
 const startMidnightDailyJob = async () => {
   // docker exec main yarn run job
   const leaseContractUseCases = new LeaseContractUseCases();
   const leaseContracts = await leaseContractUseCases.listWorkInProgress();
-  const results: Promise<void>[] = [];
+  const results: Promise<ILeaseContract>[] = [];
 
   for (const leaseContract of leaseContracts) /* eslint-disable-line*/ {
     const { startDate, nextDate, timeType, timeAmount } = leaseContract;
