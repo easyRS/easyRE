cf7d16c7767ca08b82001814b50a62acd9cef1ad
Luis Fernando Moron <contact@luifermoron.com>
2024-03-12 11:29:15 -0400

    test: increase ram capacity

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false
-};
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer({
+  env: {
+    NEXT_PUBLIC_ENV: 'PRODUCTION' // your next configs goes here
+  }
+});
